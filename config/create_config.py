b7c1bb74c3356d156b1e844db4cf0c18d5d6d979
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-11-17 21:18:34 -0300

    Update create_config.py

--- config/create_config.py
+++ config/create_config.py
@@ -4,22 +4,24 @@ import json
 name_list = ["cable_cam", "closet_tour", "pac_man", "sunset"]
 projection_list = ['cmp', 'erp']
 tiling_list = ['1x1', '3x2', '6x4', '9x6', '12x8']
+quality_list = ["22", "28", "34"]
 
 for name in name_list:
     for projection in projection_list:
-        for tiling in tiling_list:
-            daa = {
-                "video": f"{name}",
-                "projection": f"{projection}",
-                "tiling": f"{tiling}",
-                "resolution": "360x240",
-                "fov_resolution": "293x240",
-                "fov": "110x90",
-                "duration": 60,
-                "fps": 30,
-                "quality_list": ["22", "28", "34"],
-                "segment_template": f"../decodable_lo/{name}/{projection}/{tiling}/" + "tile{tile}/qp{quality}/chunk{chunk}.mp4",
-                "head_movement_filename": "dataset/head_movement.hd5",
-            }
-            file_base = Path(f"{name}_{projection}_{tiling}.json")
-            file_base.write_text(json.dumps(daa, indent=4))
+        daa = {
+            "video_list": [f"{name}"],
+            "projection_list": [f"{projection}"],
+            "tiling_list": f"{tiling_list}",
+            "quality_list": f"{quality_list}",
+            "rate_control": "qp",
+            "resolution": "360x240",
+            "fov_resolution": "293x240",
+            "fov": "110x90",
+            "duration": 60,
+            "fps": 30,
+            "gop": 30,
+            "segment_template": "../decodable_lo/{name}/{projection}/{tiling}/tile{tile}/{rate_control}{quality}/chunk{chunk}.mp4",
+            "head_movement_filename": "dataset/head_movement.hd5",
+        }
+        file_base = Path(f"{name}_{projection}.json")
+        file_base.write_text(json.dumps(daa, indent=4))
