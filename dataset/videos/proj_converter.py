59b6ce59914c79cacf7fcb14dd377e5a8d04e9bb
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-11-17 21:18:38 -0300

    Update proj_converter.py

--- dataset/videos/proj_converter.py
+++ dataset/videos/proj_converter.py
@@ -1,29 +1,35 @@
-from py360tools import ERP, CMP, Viewport, TileStitcher, ReadVideo
 from pathlib import Path
+
+import numpy as np
 from PIL import Image
+from py360tools import CMP, ERP, ReadVideo
+
 
 path: Path = Path(f'video_name.mp4')
 projection: str
 
+
 def parse_cmd():
+    ...
+
 
 def main():
     v_reader = ReadVideo(path, gray=False, dtype='int8')
-    cmp = CMP(proj_res='4320x2160', tiling='3x2')
-    if projection == 'erp':
-        proj_obj = ERP(proj_res='4320x2160', tiling='1x1')
-    elif projection == 'cmp':
-            proj_obj = CMP(proj_res='3240x2160', tiling='1x1')
-    else:
-        raise ValueError('Invalid projection type')
+    erp = ERP(proj_res='4320x2160')
+    cmp = CMP(proj_res='3240x2160')
 
-    proj_obj.tile_list[0].path = path
-    TileStitcher()
+    for n, proj_frame in enumerate(v_reader):
+        cmp_coord = cmp.xyz2nm(erp.xyz)
 
-    stitcher = TileStitcher(vptiles, erp)
-    for n, proj_frame in enumerate(stitcher):
-        viewport = vp.extract_viewport(proj_frame)
-        vp_image = Image.fromarray(viewport)
+        cmp_coord.transpose((1, 2, 0))
+        saida = np.zeros((200, 100))
+        # import cv2
+        # cv2.remap(src=proj_frame,
+        #           map1=cmp_coord[1], map2=cmp_coord[0],
+        #           interpolation=cv2.INTER_LINEAR,
+        #           borderMode=cv2.BORDER_WRAP,
+        #           dst=saida)
+        vp_image = Image.fromarray(saida)
         vp_image.save(f'vp_image_frame{n}.png')
 
 
